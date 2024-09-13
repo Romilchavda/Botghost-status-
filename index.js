32f34734987f9dbaa05a04a8de085bcfa3260d3b
Romil Chavda <89027751+Romilchavda@users.noreply.github.com>
2024-09-13 20:42:22 +0530

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Dragon's Den 🐲", "🎮 Playing With Dragon's 🐉"];
+const statusMessages = ["Listening to Dragon's Den 🐲", "Playing With Dragon's 🐉"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
